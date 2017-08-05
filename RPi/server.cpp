ab08ff5abdb239c35f3d39e8a1a8e8c2ad932157
Stephan Pöhland <stephan-poehland@web.de>
2017-08-05 13:34:57 +0200

    integration of ProtocolLibrary

--- RPi/server.cpp
+++ RPi/server.cpp
@@ -5,29 +5,28 @@
 #include "ProtocolLibrary.h"
 
 //MessageLength
-#define ML 1056
+//#define ML 1056
 
 using std::cout;
 using std::endl;
 
 using namespace std;
 
-struct Message{
+/*struct Message{
 	Message() : transferFailure(false), request(true) {}
 	bool transferFailure;
 	bool request;
 	string command;
 	string value;
-};
+};*/
 
-Message extractHeader(string value);
 bool handleRequest(string command, string value);
 bool handleAnswer(string command, string value);
 
 int main() 
 {
 	char recvdValue[10000];
-	init("192.168.1.1", "5001");
+	init((char *)"192.168.1.1", (char *) "5001");
 	
 	while(true)
 	{
@@ -36,8 +35,8 @@ int main()
 		EmpfangeRobotKommando(recvdValue);
 		cout << "Value received: " << recvdValue << endl;
 		string recvdString(recvdValue);
-		Message message;
-		message = extractHeader(recvdValue);
+		ProtocolLibrary::Message message;
+		message = ProtocolLibrary::extractHeader(recvdValue);
 
 		cout << "Actual Message:" << endl;
 		cout << "\t" << "failure? " << (message.transferFailure ? "Yes" : "No") << endl;
@@ -56,58 +55,6 @@ int main()
 	}
 	
     return 0;
-}
-
-//extracts information from package to struct Message and tests for transfer failures
-Message extractHeader(string value)
-{	
-	Message mes;
-	size_t pos = value.find("_");
-	if (value.substr(0,pos).compare("start") == 0)
-	{
-		value = value.substr(pos+1);
-		pos = value.find("_");
-		if (value.substr(0,pos).compare("request") == 0)
-		{
-			mes.request = true;			
-		}
-		else if (value.substr(0,pos).compare("answer") == 0)
-		{
-			mes.request = false;
-		}
-		else
-		{
-			mes.transferFailure = true;
-			//sendrepeatrequest()
-			cout << "send repeat request not implemented yet" << endl;
-		}
-		
-		value = value.substr(pos+1);
-		pos = value.find(":");
-		mes.command = value.substr(0, pos);
-		value = value.substr(pos+1);
-		pos = value.find(":");
-		mes.value = value.substr(0, pos);
-		value = value.substr(pos+1);
-		
-		//test if end is without failure
-		string testStr = "end_";
-		testStr += ((mes.request) ? "request_" : "answer_") + mes.command + ";";
-		if (value.compare(testStr) != 0)
-		{
-			mes.transferFailure = true;
-			//sendrepeatrequest()
-			cout << "send repeat request not implemented yet" << endl;
-		}
-	}
-	else
-	{
-		mes.transferFailure = true;
-		//sendrepeatrequest()
-		cout << "send repeat request not implemented yet" << endl;
-	}
-
-	return mes;
 }
 
 bool handleRequest(string command, string value)
